b09d73fece1d421f6f5d3582e33246fe02092261
vinnietito <vincentnjuguna69@gmail.com>
2024-11-22 00:18:31 +0300

    Commits

--- firstapp/lib/my_home_page.dart
+++ firstapp/lib/my_home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 //import 'package:audio/app_colors.dart' as AppColors;
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key}) : super(key: key);
+  const MyHomePage({required Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
