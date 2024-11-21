f836bbc761802b092c4eff8ad6dce57091387d4e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-22 00:53:07 +0300

    Commits

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       title: 'Flutter Audio Reading',
-      debugShowCheckedModeBanner: false;
 
       theme: ThemeData(
 
