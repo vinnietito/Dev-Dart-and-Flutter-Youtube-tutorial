6c78ca759bfdebbdbd18e662228d0c5d87811ab5
vinnietito <vincentnjuguna69@gmail.com>
2024-11-21 23:57:00 +0300

    Commits

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
