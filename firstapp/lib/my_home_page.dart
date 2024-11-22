7e9ce814bebf143c363cb1912cfada71d8b7afdb
vinnietito <vincentnjuguna69@gmail.com>
2024-11-22 18:12:03 +0300

    Commits

--- firstapp/lib/my_home_page.dart
+++ firstapp/lib/my_home_page.dart
@@ -1,55 +1,56 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:audio/app_colors.dart' as AppColors;
+import "package:audio/app_colors.dart" as AppColors;
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({required Key key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class  _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      color: AppColors.background,
-          child: safeArea(
-              child: Scaffold(
-                body: Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(left: 20, right:20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ImageIcon(AssetImage("img/menu.png"), size: 24, color: Colors.black,)
-                          Row(
-                            children: [
-                              Icon(Icons.search),
-                              SizedBox(width: 10,)
-                              Icon(Icons.notifications),
-                            ],
-                          )
-                          Row(
-                            children: [
-                              Container(
-                                SizedBox(height: 20,),
-                                margin: const EdgeInsets.only(left: 20),
-                                child: Text("Popular Books", style: TextStyle(fontSize: 30)),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-
-                    )
-
-                  ],
-                ),
+    return Scaffold(
+      backgroundColor: AppColors.background, // Assuming this color is defined
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Top Row (Menu, Search, Notifications)
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const ImageIcon(
+                    AssetImage("img/menu.png"), // Ensure the asset is added in pubspec.yaml
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.search),
+                      SizedBox(width: 10),
+                      Icon(Icons.notifications),
+                    ],
+                  ),
+                ],
               ),
-          ),
+            ),
+            const SizedBox(height: 20), // Spacing below the top row
+
+            // Title Row (Popular Books)
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "Popular Books",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
-  safeArea({required child}) {}
 }
