74faca78a5cb6aa32789124863627ee3ce26d099
vinnietito <vincentnjuguna69@gmail.com>
2024-11-22 20:24:11 +0300

    Commits

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'my_home_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,28 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Test App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello, World!',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Icon(
-                Icons.thumb_up,
-                size: 50,
-                color: Colors.blue,
-              ),
-            ],
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter EBook App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
   }
 }
