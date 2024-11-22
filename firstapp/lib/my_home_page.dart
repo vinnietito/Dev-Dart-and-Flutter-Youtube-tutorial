a64fc686c10728bcf0001ba1a1aab11553c14350
vinnietito <vincentnjuguna69@gmail.com>
2024-11-22 15:35:15 +0300

    Commits

--- firstapp/lib/my_home_page.dart
+++ firstapp/lib/my_home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:audio/app_colors.dart' as AppColors;
+import 'package:audio/app_colors.dart' as AppColors;
 class MyHomePage extends StatefulWidget {
   const MyHomePage({required Key key}) : super(key: key);
 
@@ -12,11 +12,20 @@ class  _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return  Container(
-      color: ,
-            child: safeArea(
+      color: AppColors.background,
+          child: safeArea(
               child: Scaffold(
+                body: Column(
+                  children: [
+                    Row(
+                      children: [
+                        ImageIcon(AssetImage("img/menu.png"))
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
+          ),
     );
   }
 
